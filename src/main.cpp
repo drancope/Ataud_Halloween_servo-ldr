4d0bc6ef15aaf4de42d78e739e70afa0f1a00cf4
Antonio Contreras <drancope@gmail.com>
2023-10-30 01:09:43 +0100

    nuevo env esp8266

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,22 @@
 Servo myservo;  // create servo object to control a servo
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(3);  // attaches the servo on pin 9 to the servo object
   pinMode(A0, INPUT);
-  //Serial.begin(115200);
+  Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   int valor = analogRead(A0);
-  //Serial.print("sensor = ");
-  //Serial.println(valor);
+  Serial.print("sensor = ");
+  Serial.println(valor);
    if (valor > 600) {
     myservo.write(10);                  // sets the servo position according to the scaled value
     delay(2015);      
     digitalWrite(LED_BUILTIN, HIGH);
   } else {                    // waits for the servo to get there
-    myservo.write(100);                  // sets the servo position according to the scaled value                          // waits for the servo to get there
+    myservo.write(100);                   // sets the servo position according to the scaled value                          // waits for the servo to get there
     digitalWrite(LED_BUILTIN, LOW);
   }
   delay(500);
