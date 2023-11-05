b9bf14a43d19758b67ee914b59552cd3cc7086c1
Antonio Contreras <drancope@gmail.com>
2023-11-05 21:05:30 +0100

    Último upload: pro-mini

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,19 @@
 #define TARGET 1 // 0 = nano; 1 = pro-mini; 2 = esp8266
 #if TARGET == 0
   #define UMBRAL 200
+  #define PINMOTOR 9
 #elif TARGET == 1
   #define UMBRAL 800
+  #define PINMOTOR 9
 #else
   #define UMBRAL 200
+  #define PINMOTOR 3
 #endif
 
 Servo myservo;  // create servo object to control a servo
 
 void setup() {
-  myservo.attach(3);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(PINMOTOR);  // attaches the servo on pin 9 to the servo object
   pinMode(A0, INPUT);
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
@@ -24,11 +27,11 @@ void loop() {
   Serial.print("sensor = ");
   Serial.println(valor);
    if (valor > UMBRAL) {
-    myservo.write(10);                  // sets the servo position according to the scaled value
-    delay(2015);      
+    myservo.write(5);                  // sets the servo position according to the scaled value
     digitalWrite(LED_BUILTIN, HIGH);
+    delay(2015);      
   } else {                    // waits for the servo to get there
-    myservo.write(100);                   // sets the servo position according to the scaled value                          // waits for the servo to get there
+    myservo.write(160);                   // sets the servo position according to the scaled value                          // waits for the servo to get there
     digitalWrite(LED_BUILTIN, LOW);
   }
   delay(500);
